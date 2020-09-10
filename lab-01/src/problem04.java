f24578726720bca625f15b574d2d38402ae1e989
Orozkul Oktonov <oktonov_o@auca.kg>
2020-09-10 15:18:40 +0600

    problem 5

--- lab-01/src/problem04.java
+++ lab-01/src/problem04.java
@@ -5,7 +5,7 @@ public class problem04 {
         Scanner inp = new Scanner(System.in);
         System.out.print("1st int? ");
         int firstInt = inp.nextInt();
-        System.out.print("2st int? ");
+        System.out.print("2nd int? ");
         int secondInt = inp.nextInt();
         System.out.println(firstInt+" + "+secondInt+" = "+firstInt+secondInt);
         System.out.print(firstInt+" - "+secondInt+" = ");System.out.println(firstInt-secondInt);
---
+++ lab-01/src/problem05.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class problem05 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("1st double? ");
+        double firstDouble = inp.nextDouble();
+        System.out.print("2nd double? ");
+        double secondDouble = inp.nextDouble();
+        System.out.println(firstDouble+" + "+secondDouble+" = "+firstDouble+secondDouble);
+        System.out.print(firstDouble+" - "+secondDouble+" = ");System.out.println(firstDouble-secondDouble);
+        System.out.println(firstDouble+" * "+secondDouble+" = "+firstDouble*secondDouble);
+        System.out.println(firstDouble+" / "+secondDouble+" = "+firstDouble/secondDouble);
+        System.out.println(firstDouble+" % "+secondDouble+" = "+firstDouble%secondDouble);
+
+    }
+}
