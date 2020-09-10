dde9636542debb1a68d3fd005a7623cbeb911faa
Orozkul Oktonov <oktonov_o@auca.kg>
2020-09-10 15:58:02 +0600

    make up

--- lab-01/src/problem03.java
+++ lab-01/src/problem03.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class problem03 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        System.out.println("What is your name? ");
+        System.out.print("What is your name? ");
         String userName = inp.nextLine();
         System.out.println("Hello, "+userName+"!");
     }
