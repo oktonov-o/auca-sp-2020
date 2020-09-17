c36d20d9f0469be605be0f8daa5a2e73b3630b43
Orozkul Oktonov <oktonov_o@auca.kg>
2020-09-17 15:01:44 +0600

    URI p-1007

---
+++ URI Midterm/p-1007/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int a = inp.nextInt();
+        int b = inp.nextInt();
+        int c = inp.nextInt();
+        int d = inp.nextInt();
+        int X= a*b-c*d;
+        System.out.println("DIFERENCA = "+X);
+    }
+}
