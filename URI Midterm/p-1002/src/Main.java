4d50c578914ef91b2b8d0f7afa2f3bc229901e75
Orozkul Oktonov <oktonov_o@auca.kg>
2020-09-17 14:38:50 +0600

    URI p-1002 and p-1003

---
+++ URI Midterm/p-1002/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+            Scanner inp = new Scanner(System.in);
+            double r = inp.nextDouble();
+            double a = 3.14159*r*r;
+            System.out.printf("A=%.4f%n",a);
+    }
+}
+
