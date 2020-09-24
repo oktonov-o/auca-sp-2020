6233477676950b33bf1a7e2b3a8709ba4b77b298
Orozkul Oktonov <oktonov_o@auca.kg>
2020-09-24 15:23:18 +0600

    Problem04

--- lab-02/src/Problem04.java
+++ lab-02/src/Problem04.java
@@ -4,8 +4,8 @@ public class Problem04 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         System.out.print("Length in inches? ");
-        int inch=inp.nextInt();
-        System.out.println(inch+" in. = "+inch*2.54+" cm.");
+        int inch = inp.nextInt();
+        System.out.println(inch + " in. = " + inch * 2.54 + " cm.");
 
     }
 }
