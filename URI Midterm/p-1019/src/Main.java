0f3209c44cb838d81c39dd3482e60461fef9997b
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-07 16:43:55 +0600

    URI p-1019-accepted

--- URI Midterm/p-1019/src/Main.java
+++ URI Midterm/p-1019/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int seconds = inp.nextInt();
-        int hours = seconds/60/60;
-        int minutes = seconds/60%60;
+        int hours = seconds / 60 / 60;
+        int minutes = seconds / 60 % 60;
         seconds %= 60;
-        System.out.printf("%d:%d:%d%n", hours,minutes,seconds);
+        System.out.printf("%d:%d:%d%n", hours, minutes, seconds);
     }
 }
