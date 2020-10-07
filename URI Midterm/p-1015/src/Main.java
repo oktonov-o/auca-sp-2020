8085462b11a565dfbba60d1e2372ca40d675d7f9
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-07 16:47:13 +0600

    URI p-1015-accepted

--- URI Midterm/p-1015/src/Main.java
+++ URI Midterm/p-1015/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         double[] point = new double[4];
-        double x1,x2,y1,y2;
-        int i=0;
-        while (inp.hasNext()){
+        double x1, x2, y1, y2;
+        int i = 0;
+        while (inp.hasNext()) {
             point[i] = inp.nextDouble();
             i++;
         }
@@ -14,7 +14,7 @@ public class Main {
         y1 = point[1];
         x2 = point[2];
         y2 = point[3];
-        double ans = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        double ans = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
         System.out.printf("%.4f%n", ans);
     }
 }
