e664396ff9a213161af2c9ebf4dc87b747a093f2
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-07 13:17:40 +0600

    updated URI problems

--- URI Midterm/p-1015/src/Main.java
+++ URI Midterm/p-1015/src/Main.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        int km = inp.nextInt();
-        System.out.println(2*km+" minutos");
+        double[] point = new double[4];
+        double x1,x2,y1,y2;
+        int i=0;
+        while (inp.hasNext()){
+            point[i] = inp.nextDouble();
+            i++;
+        }
+        x1 = point[0];
+        y1 = point[1];
+        x2 = point[2];
+        y2 = point[3];
+        double ans = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.printf("%.4f%n", ans);
     }
 }
---
+++ URI Midterm/p-1016/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int km = inp.nextInt();
+        System.out.println(2*km+" minutos");
+    }
+}
