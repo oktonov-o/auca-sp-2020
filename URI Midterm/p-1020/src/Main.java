f87f04fdf0ad03e04d4e6754440fe34293c8c3a4
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-07 16:42:07 +0600

    URI p-1021-accepted

--- URI Midterm/p-1020/src/Main.java
+++ URI Midterm/p-1020/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int days = inp.nextInt();
-        int year = days/365;
-        int month = days%365/30;
-        days = days%365%30;
-        System.out.printf("%d ano(s)%n%d mes(es)%n%d dia(s)%n", year,month,days);
+        int year = days / 365;
+        int month = days % 365 / 30;
+        days = days % 365 % 30;
+        System.out.printf("%d ano(s)%n%d mes(es)%n%d dia(s)%n", year, month, days);
     }
 }
