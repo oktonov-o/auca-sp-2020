f7fe1f4e66977d0f44fd549df29229f1feba82fa
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-07 13:20:10 +0600

    URI problem 1017

---
+++ URI Midterm/p-1017/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        double hours = inp.nextDouble();
+        double kmPerHour = inp.nextDouble();
+        double lit = hours*kmPerHour/12;
+        System.out.printf("%.3f%n", lit);
+    }
+}
