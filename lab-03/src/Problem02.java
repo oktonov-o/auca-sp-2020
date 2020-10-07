b9112473d57b70bd0d4a5cb0c96d9a3e27c1c0ce
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-07 16:54:00 +0600

    lab-03 Problem02 final

--- lab-03/src/Problem02.java
+++ lab-03/src/Problem02.java
@@ -4,7 +4,7 @@ public class Problem02 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         System.out.print("Number of points? ");
-        double number = inp.nextDouble();
+        double number= inp.nextDouble();
         if (number >= 40) {
             System.out.println("You passed \"Structured Programming\"!!!");
             System.out.println("You should take \"Object-Oriented Programming\" in the next semester");
