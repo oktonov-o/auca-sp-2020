f610dd8a2ad1b3982b473629589fc55ec94d51af
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-07 14:02:17 +0600

    problem01

---
+++ lab-03/src/Problem01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem01 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("some real number? ");
+        double number = inp.nextDouble();
+        if(number>=0){
+            System.out.println("|"+number+"| = "+number);
+        } else {
+            System.out.println("|"+number+"| = "+(-1)*number);
+        }
+    }
+}
---
+++ lab-03/src/Problem02.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+    }
+}
