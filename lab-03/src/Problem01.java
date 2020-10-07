c6023fb2983a24b0015f6e2c4e8957084b8b34d3
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-07 16:53:34 +0600

    lab-03 Problem01 final

--- lab-03/src/Problem01.java
+++ lab-03/src/Problem01.java
@@ -4,7 +4,7 @@ public class Problem01 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         System.out.print("some real number? ");
-        double number = inp.nextDouble();
+        double number= inp.nextDouble();
         if (number >= 0) {
             System.out.println("|" + number + "| = " + number);
         } else {
