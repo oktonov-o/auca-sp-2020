f0e0ac2777ce4aa6a69810e6774c48fcf27300ae
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-07 14:29:16 +0600

    problem03

--- lab-03/src/Problem02.java
+++ lab-03/src/Problem02.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class Problem02 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-
+        System.out.print("Number of points? ");
+        double number = inp.nextDouble();
+        if(number>=40){
+            System.out.println("You passed \"Structured Programming\"!!!");
+            System.out.println("You should take \"Object-Oriented Programming\" in the next semester");
+        } else {
+            System.out.println("You failed \"Structured Programming\"!!!");
+            System.out.println("You can take it again in the fall semester next year.");
+        }
     }
 }
--- lab-03/src/Problem03.java
+++ lab-03/src/Problem03.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 public class Problem03 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-
+        System.out.print("1st number? ");
+        int a = inp.nextInt();
+        System.out.print("2nd number? ");
+        int b = inp.nextInt();
+        System.out.print("3rd number? ");
+        int c = inp.nextInt();
+        if (a>=b && a>=c){
+            System.out.printf("The value %d is the greatest one.", a);
+        } else if (b>=a && b>=c){
+            System.out.printf("The value %d is the greatest one.", b);
+        } else {
+            System.out.printf("The value %d is the greatest one.", c);
+        }
     }
 }
