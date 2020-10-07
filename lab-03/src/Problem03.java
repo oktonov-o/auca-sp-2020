8c212e06990146b85dd00c6cc239c260017d12ff
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-07 16:55:58 +0600

    lab-03 Problem03 final

--- lab-03/src/Problem03.java
+++ lab-03/src/Problem03.java
@@ -4,11 +4,11 @@ public class Problem03 {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         System.out.print("1st number? ");
-        int a = inp.nextInt();
+        int a= inp.nextInt();
         System.out.print("2nd number? ");
-        int b = inp.nextInt();
+        int b= inp.nextInt();
         System.out.print("3rd number? ");
-        int c = inp.nextInt();
+        int c= inp.nextInt();
         if (a >= b && a >= c) {
             System.out.printf("The value %d is the greatest one.", a);
         } else if (b >= a && b >= c) {
