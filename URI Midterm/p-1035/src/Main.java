4dccee77295003b26574111ebeee534252ef6e00
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-19 20:57:08 +0600

    URI p-1035-accepted

--- URI Midterm/p-1035/src/Main.java
+++ URI Midterm/p-1035/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
+        Scanner inp =new Scanner(System.in);
         int a = inp.nextInt();
         int b = inp.nextInt();
         int c = inp.nextInt();
