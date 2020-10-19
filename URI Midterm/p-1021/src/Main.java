93996a7a2bb046acd42f3d11b01ff3fa7ff6962e
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-19 20:51:47 +0600

    URI p-1021-accepted

--- URI Midterm/p-1021/src/Main.java
+++ URI Midterm/p-1021/src/Main.java
@@ -30,18 +30,18 @@ public class Main {
         int c1 =x;
 
         System.out.println("NOTAS:");
-        System.out.printf("%d nota(s) de R$ 100,00%n", n100);
-        System.out.printf("%d nota(s) de R$ 50,00%n", n50);
-        System.out.printf("%d nota(s) de R$ 20,00%n", n20);
-        System.out.printf("%d nota(s) de R$ 10,00%n", n10);
-        System.out.printf("%d nota(s) de R$ 5,00%n", n5);
-        System.out.printf("%d nota(s) de R$ 2,00%n", n2);
+        System.out.printf("%d nota(s) de R$ 100.00%n", n100);
+        System.out.printf("%d nota(s) de R$ 50.00%n", n50);
+        System.out.printf("%d nota(s) de R$ 20.00%n", n20);
+        System.out.printf("%d nota(s) de R$ 10.00%n", n10);
+        System.out.printf("%d nota(s) de R$ 5.00%n", n5);
+        System.out.printf("%d nota(s) de R$ 2.00%n", n2);
         System.out.println("MOEDAS:");
-        System.out.printf("%d moeda(s) de R$ 1,00%n", c100);
-        System.out.printf("%d moeda(s) de R$ 0,50%n", c50);
-        System.out.printf("%d moeda(s) de R$ 0,25%n", c25);
-        System.out.printf("%d moeda(s) de R$ 0,10%n", c10);
-        System.out.printf("%d moeda(s) de R$ 0,05%n", c5);
-        System.out.printf("%d moeda(s) de R$ 0,01%n", c1);
+        System.out.printf("%d moeda(s) de R$ 1.00%n", c100);
+        System.out.printf("%d moeda(s) de R$ 0.50%n", c50);
+        System.out.printf("%d moeda(s) de R$ 0.25%n", c25);
+        System.out.printf("%d moeda(s) de R$ 0.10%n", c10);
+        System.out.printf("%d moeda(s) de R$ 0.05%n", c5);
+        System.out.printf("%d moeda(s) de R$ 0.01%n", c1);
     }
 }
