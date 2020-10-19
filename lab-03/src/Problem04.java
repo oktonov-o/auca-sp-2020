4a97cbfd10cdb047d97380b20f43ade3a465622b
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-19 22:58:10 +0600

    lab-03 Problem04 final

---
+++ lab-03/src/Problem04.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Problem04 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Year? ");
+        int year = inp.nextInt();
+        if (year % 400 == 0 || year % 100 != 0 && year % 4 == 0)
+            System.out.println("This is a leap year.");
+        else
+            System.out.println("This is not a leap year.");
+    }
+}
