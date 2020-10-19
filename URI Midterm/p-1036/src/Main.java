f1bd796ba47d83a4b2e99d65c518e4098954cb4b
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-19 21:28:09 +0600

    URI p-1036-accepted

--- URI Midterm/p-1036/src/Main.java
+++ URI Midterm/p-1036/src/Main.java
@@ -11,11 +11,11 @@ public class Main {
         if (det < 0 || a == 0){
             System.out.println("Impossivel calcular");
         } else if ( det == 0){
-            root = -b/2*a;
+            root = -b/(2*a);
             System.out.printf("R = %.5f%n", root);
         } else {
-            root1 = (Math.sqrt(det)-b)/2*a;
-            root2 = (-Math.sqrt(det)-b)/2*a;
+            root1 = (Math.sqrt(det)-b)/(2*a);
+            root2 = (-Math.sqrt(det)-b)/(2*a);
             System.out.printf("R1 = %.5f%n", root1);
             System.out.printf("R2 = %.5f%n", root2);
         }
