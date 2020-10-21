17dbdff16e0f7b1e67ec981187d0dfd7d25899aa
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-21 14:46:36 +0600

    URI p-1044-in a queue

---
+++ URI Midterm/p-1044/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int a = inp.nextInt();
+        int b = inp.nextInt();
+        if (b%a==0)
+            System.out.println("Sao Multiplos");
+        else
+            System.out.println("Nao sao Multiplos");
+    }
+}
