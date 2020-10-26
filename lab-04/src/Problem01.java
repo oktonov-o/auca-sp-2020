c2d078bc51bc1f87e6f416d87c4105e3f0a0829b
oktonov-o <oktonov_o@auca.kg>
2020-10-26 22:39:18 +0600

    Problem01-final

--- lab-04/src/Problem01.java
+++ lab-04/src/Problem01.java
@@ -1,1 +1,12 @@
+public class Main {
 
+  public static void main(String[] args) {
+        double d = 0.1;
+        double sum = 1e-16+d+d+d+d+d+d+d+d+d+d;
+        if (sum == 1.0){
+          System.out.println("Equal");
+        } else {
+          System.out.println("Not equal");
+        }
+  }
+}
