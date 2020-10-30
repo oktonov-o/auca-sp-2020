a51c9c5ede39bdb30926c4ebadf38039c4894897
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-30 21:38:26 +0600

    lab-04 Problem06 final

--- lab-04/src/Problem06.java
+++ lab-04/src/Problem06.java
@@ -6,10 +6,10 @@ public class Problem06 {
         System.out.print("Enter a point with two coordinates: ");
         double x = inp.nextDouble();
         double y = inp.nextDouble();
-        if (x >= -5 && x <= 5 && y >= -2.5 && y <= 2.5){
-            System.out.println("Point ("+x+", "+y+") is in the rectangle");
+        if (x >= -5 && x <= 5 && y >= -2.5 && y <= 2.5) {
+            System.out.println("Point (" + x + ", " + y + ") is in the rectangle");
         } else {
-            System.out.println("Point ("+x+", "+y+") is not in the rectangle");
+            System.out.println("Point (" + x + ", " + y + ") is not in the rectangle");
         }
     }
 }
