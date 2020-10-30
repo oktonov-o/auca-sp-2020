d1be5a0ab183d3c0e7695a70adc7320ac9f32a9c
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-30 22:51:10 +0600

    lab-04 Problem07 final

--- lab-04/src/Problem07.java
+++ lab-04/src/Problem07.java
@@ -12,15 +12,19 @@ public class Problem07 {
         n%=100;
         user[1] = n/10;
         user[2] = n%10;
+        int a = user [0];
+        int b = user [1];
+        int c = user [2];
         for (int i=0; i<3; i++){
             for ( int j=0; j<3; j++){
                 if(lottery[i] == user[j]){
                     cnt++;
+                    user[j]=-1;
                     break;
                 }
             }
         }
-        if (lottery[0]==user[0] && lottery[1]==user[1] && lottery[2]==user[2]){
+        if (lottery[0]==a && lottery[1]==b && lottery[2]==c){
             System.out.println("Your award is $10,000");
         } else if (cnt == 3){
             System.out.println("Your award is $3,000");
---
+++ lab-04/src/Problem08.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Problem08 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+    }
+}
