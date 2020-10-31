f8a14534c1c35f2c620961e1102dbf9c095bf27f
Orozkul Oktonov <oktonov_o@auca.kg>
2020-10-31 20:30:25 +0600

    URI p-1046-accepted

--- URI Midterm/p-1046/src/Main.java
+++ URI Midterm/p-1046/src/Main.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int start = inp.nextInt();
         int finish = inp.nextInt();
         int time;
         if (start >= finish){
-          time = 24+finish-start;
+            time = 24+finish-start;
         } else {
-          time = finish - start;
+            time = finish - start;
         }
         System.out.printf("O JOGO DUROU %d HORA(S)%n", time);
-  }
+    }
 }
