104e1817b73dc3a96fab5ffb8cea089e0a5579f2
Orozkul Oktonov <oktonov_o@auca.kg>
2020-11-01 18:18:43 +0600

    URI p-1072-in queue

---
+++ URI Midterm/p-1072/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        int x, cntIn =0, cntOut = 0;
+        for (int i=0; i<n; i++){
+            x = inp.nextInt();
+            if (x>= 10 && x<=20){
+                cntIn++;
+            } else {
+                cntOut++;
+            }
+        }
+        System.out.println(cntIn+" in");
+        System.out.println(cntOut+" out");
+    }
+}
