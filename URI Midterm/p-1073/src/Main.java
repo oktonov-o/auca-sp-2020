5b644689562dcaaa87847668ecbebcdf7b7c2b3f
Orozkul Oktonov <oktonov_o@auca.kg>
2020-11-01 18:28:52 +0600

    URI p-1073-in queue

---
+++ URI Midterm/p-1073/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        for (int i=2; i<=n; i+=2){
+            System.out.println(i+"^2 = "+i*i);
+        }
+    }
+}
