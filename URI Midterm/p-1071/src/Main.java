b594eca00c0c38e86223e73703d8e29d61e07a3c
Orozkul Oktonov <oktonov_o@auca.kg>
2020-11-01 18:09:35 +0600

    URI p-1071-in queue

---
+++ URI Midterm/p-1071/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int x = inp.nextInt();
+        int y = inp.nextInt();
+        int sum = 0;
+        if (y%2==0){
+            for (int i = y+1; i < x; i+=2){
+                sum += i;
+            }
+        } else {
+            for (int i = y+2; i < x; i+=2){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
