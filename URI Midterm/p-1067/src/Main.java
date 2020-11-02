e9de0ab6b5ae0a05b9065b1baec682b77d880726
Orozkul Oktonov <oktonov_o@auca.kg>
2020-11-02 10:23:48 +0600

    URI p-1067-accepted

--- URI Midterm/p-1067/src/Main.java
+++ URI Midterm/p-1067/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int n = inp.nextInt();
-        for (int i=1; i<=n; i+=2){
+        for (int i = 1; i <= n; i += 2) {
             System.out.println(i);
         }
     }
