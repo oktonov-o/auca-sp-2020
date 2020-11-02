3b98f061563cf56afa20497b4d0d0a90f73d5b4e
Orozkul Oktonov <oktonov_o@auca.kg>
2020-11-02 10:21:00 +0600

    URI p-1073-accepted

--- URI Midterm/p-1073/src/Main.java
+++ URI Midterm/p-1073/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int n = inp.nextInt();
-        for (int i=2; i<=n; i+=2){
-            System.out.println(i+"^2 = "+i*i);
+        for (int i = 2; i <= n; i += 2) {
+            System.out.println(i + "^2 = " + i * i);
         }
     }
 }
