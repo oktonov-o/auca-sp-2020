7db37b8fb516fada67951fd7c018d8a7628e974c
Orozkul Oktonov <oktonov_o@auca.kg>
2020-11-02 10:23:09 +0600

    URI p-1070-accepted

--- URI Midterm/p-1070/src/Main.java
+++ URI Midterm/p-1070/src/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int n = inp.nextInt();
-        if (n % 2 == 0){
-            for (int i=n+1; i<=n+11; i+=2){
+        if (n % 2 == 0) {
+            for (int i = n + 1; i <= n + 11; i += 2) {
                 System.out.println(i);
             }
         } else {
-            for (int i=n; i<= n+10; i+=2){
+            for (int i = n; i <= n + 10; i += 2) {
                 System.out.println(i);
             }
         }
