a1a4fdf66c151125be0cc6cb8ac5ba68efbbf67a
Orozkul Oktonov <oktonov_o@auca.kg>
2020-11-02 10:22:31 +0600

    URI p-1071-accepted

--- URI Midterm/p-1071/src/Main.java
+++ URI Midterm/p-1071/src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         int x = inp.nextInt();
         int y = inp.nextInt();
         int sum = 0;
-        if (y%2==0){
-            for (int i = y+1; i < x; i+=2){
+        if (y % 2 == 0) {
+            for (int i = y + 1; i < x; i += 2) {
                 sum += i;
             }
         } else {
-            for (int i = y+2; i < x; i+=2){
+            for (int i = y + 2; i < x; i += 2) {
                 sum += i;
             }
         }
