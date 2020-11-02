65455e307f7a378483768cb037f7b7973f4a67ed
Orozkul Oktonov <oktonov_o@auca.kg>
2020-11-02 10:29:41 +0600

    URI p-1066-accepted

--- URI Midterm/p-1066/src/Main.java
+++ URI Midterm/p-1066/src/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int x, cntEven = 0, cntOdd = 0, cntPos = 0, cntNeg = 0;
-        while (inp.hasNext()){
+        while (inp.hasNext()) {
             x = inp.nextInt();
-            if (x % 2 == 0){
+            if (x % 2 == 0) {
                 cntEven++;
             } else {
                 cntOdd++;
             }
-            if (x > 0){
+            if (x > 0) {
                 cntPos++;
-            } else if (x < 0){
+            } else if (x < 0) {
                 cntNeg++;
             }
         }
