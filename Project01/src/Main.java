6636d083f452cd5a5a23b03c7b9428323e28343d
Orozkul Oktonov <oktonov_o@auca.kg>
2020-11-29 15:47:59 +0600

    template for draw in processing

---
+++ Project01/src/Main.java
@@ -0,0 +1,20 @@
+import processing.core.*;
+
+public class Main extends PApplet {
+
+    public void settings() {
+        super.settings();
+    }
+
+    public void setup() {
+        super.setup();
+    }
+
+    public void draw() {
+        super.draw();
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+}
