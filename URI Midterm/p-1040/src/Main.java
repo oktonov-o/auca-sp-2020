122fdfdf4652fed659c5e0fda14d669d8777f9ac
Orozkul Oktonov <oktonov_o@auca.kg>
2020-11-29 15:49:18 +0600

    URI p-1040-accepted

--- URI Midterm/p-1040/src/Main.java
+++ URI Midterm/p-1040/src/Main.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        double a = inp.nextDouble();
-        double b = inp.nextDouble();
-        double c = inp.nextDouble();
-        double d = inp.nextDouble();
-        double media = (a+b+c+d)/4;
+        float a = inp.nextFloat();
+        float b = inp.nextFloat();
+        float c = inp.nextFloat();
+        float d = inp.nextFloat();
+        float median = (a*2+b*3+c*4+d)/10;
 
-        System.out.printf("Media: %.1f%n", media);
+        System.out.printf("Media: %.1f%n", median);
 
-        if(media >= 7.0){
+        if(median >= 7.0){
             System.out.println("Aluno aprovado.");
-        } else if (media < 5.0){
+        } else if (median <= 4.9){
             System.out.println("Aluno reprovado.");
-        } else {
+        } else if (median <= 6.9 && median >= 5.0) {
             System.out.println("Aluno em exame.");
             System.out.print("Nota do exame: ");
-            double exam = inp.nextDouble();
-            media = (media + exam)/2;
-            if(media >=5.0){
+            float exam = inp.nextFloat();
+            median = (median + exam)/2;
+            if(median >= 5.0){
                 System.out.println("Aluno aprovado.");
-            } else {
+            } else if(median <= 4.9) {
                 System.out.println("Aluno reprovado.");
             }
-            System.out.printf("Media final: %.1f%n", media);
+            System.out.printf("Media final: %.1f%n", median);
         }
     }
 }
