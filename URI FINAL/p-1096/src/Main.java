03846ab7eeca0e30632ec112dd9b01fe089a01ae
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-01 00:52:53 +0600

    URI p-1096 accepted

---
+++ URI FINAL/p-1096/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int j = 1; j<=9; j+=2){
+            System.out.printf("I=%d J=7%n", j);
+            System.out.printf("I=%d J=6%n", j);
+            System.out.printf("I=%d J=5%n", j);
+        }
+    }
+}
