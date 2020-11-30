fe142de54109c2f6773bde95213e4f57b8892f0c
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-01 00:45:21 +0600

    URI p-1095 accepted

---
+++ URI FINAL/p-1095/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i=1;
+        for (int j = 60; j>=0; j-=5){
+            System.out.printf("I=%d J=%d%n", i, j);
+            i+=3;
+        }
+    }
+}
