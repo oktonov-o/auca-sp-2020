fed8541b50bae37cdc143fdd0df95357c7871248
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-02 13:41:10 +0600

    lab-06 Problem01 beginning

---
+++ lab-06/src/Problem02.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Year: ");
+        int year = inp.nextInt();
+
+        System.out.print("Month: ");
+        int month = inp.nextInt();
+
+        System.out.println();
+    }
+}
