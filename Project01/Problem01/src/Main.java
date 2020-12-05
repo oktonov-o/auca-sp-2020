ee9ae22b727b5d6e8ea5fd4b6eee65fbc560d235
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-05 13:48:53 +0600

    Project01 Problem01 finish

--- Project01/Problem01/src/Main.java
+++ Project01/Problem01/src/Main.java
@@ -3,29 +3,30 @@ import processing.core.*;
 public class Main extends PApplet {
 
     final float RADIUS = 25;
-    final float w = 1600;
-    final float h = 1000;
     final float speed = 10;
-
-    float x1 = w / 2f;
-    float y1 = h / 2f;
-    float x2 = w / 2f + 2 * RADIUS;
-    float y2 = h / 2f + 2 * RADIUS;
-    float x3 = w / 2f + 4 * RADIUS;
-    float y3 = h / 2f + 4 * RADIUS;
-    float x1Speed = speed;
-    float y1Speed = speed;
-    float x2Speed = speed;
-    float y2Speed = speed;
-    float x3Speed = speed;
-    float y3Speed = speed;
+    float w,h;
+    float x1,x2,x3,y1,y2,y3;
+    float x1Speed,x2Speed,x3Speed,y1Speed,y2Speed,y3Speed;
 
     public void settings() {
         fullScreen();
     }
 
     public void setup() {
-
+        w = width;
+        h = height;
+        x1 = w / 2f;
+        y1 = h / 2f;
+        x2 = w / 2f + 2 * RADIUS;
+        y2 = h / 2f + 2 * RADIUS;
+        x3 = w / 2f + 4 * RADIUS;
+        y3 = h / 2f + 4 * RADIUS;
+        x1Speed = speed;
+        y1Speed = speed;
+        x2Speed = speed;
+        y2Speed = speed;
+        x3Speed = speed;
+        y3Speed = speed;
     }
 
     public void draw() {
