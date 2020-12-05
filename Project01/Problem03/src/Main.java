f120daef31e6af7986ff9983ede9fcd0ced5bc24
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-05 20:00:58 +0600

    Project01 Problem04 finish

--- Project01/Problem03/src/Main.java
+++ Project01/Problem03/src/Main.java
@@ -1,46 +1,55 @@
 import processing.core.*;
-
-import java.util.Scanner;
+import javax.swing.*;
 
 public class Main extends PApplet {
 
-    Scanner inp = new Scanner(System.in);
-
+    final float SIZE = 100;
     float firstX;
     float secondX;
-    String firstWord = inp.nextLine();
-    String secondWord = inp.nextLine();
-    double speedFirst = 0.3;
-    double speedSecond = 0.3;
+    String firstWord;
+    String secondWord;
+    double speedFirst;
+    double speedSecond;
 
     public void settings() {
         fullScreen();
     }
 
     public void setup() {
-
+        textSize(SIZE);
+        firstWord = JOptionPane.showInputDialog("First word: ");
+        secondWord = JOptionPane.showInputDialog("Second word: ");
+        firstX = 0;
+        secondX = width - textWidth(secondWord);
+        if (textWidth(firstWord)<textWidth(secondWord)){
+            speedFirst = (1.0*width-textWidth(firstWord))/(width-textWidth(secondWord))*5;
+            speedSecond = -5;
+        } else {
+            speedSecond = -(1.0*width-textWidth(secondWord))/(width-textWidth(firstWord))*5;
+            speedFirst = 5;
+        }
     }
 
     public void draw() {
         background(0);
 
         //first word
-        textSize(100);
+        fill(255,0,0);
         text(firstWord, firstX, height / 4f);
 
         //second word
-        textSize(100);
+        fill(0,255,0);
         text(secondWord, secondX, 3 * height / 4f);
-
-        if (firstX >= width - firstWord.length() || firstX <= 0) {
-            speedFirst *= (-1);
-        }
-        if (secondX >= width - secondWord.length() || secondX <= 0) {
-            speedSecond *= (-1);
-        }
 
         firstX += speedFirst;
         secondX += speedSecond;
+
+        if (firstX > width-textWidth(firstWord) || firstX < 0) {
+            speedFirst *= (-1);
+        }
+        if (secondX > width-textWidth(secondWord) || secondX < 0) {
+            speedSecond *= (-1);
+        }
     }
 
     public static void main(String[] args) {
