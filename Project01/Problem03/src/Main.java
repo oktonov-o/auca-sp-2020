ba005d2a27e0c0090173d7702627c6308175c9cb
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-06 01:07:44 +0600

    Project01 Problem03 finish

--- Project01/Problem03/src/Main.java
+++ Project01/Problem03/src/Main.java
@@ -1,9 +1,10 @@
 import processing.core.*;
+
 import javax.swing.*;
 
 public class Main extends PApplet {
 
-    final float SIZE = 100;
+    final float SIZE = 80;
     float firstX;
     float secondX;
     String firstWord;
@@ -21,11 +22,11 @@ public class Main extends PApplet {
         secondWord = JOptionPane.showInputDialog("Second word: ");
         firstX = 0;
         secondX = width - textWidth(secondWord);
-        if (textWidth(firstWord)<textWidth(secondWord)){
-            speedFirst = (1.0*width-textWidth(firstWord))/(width-textWidth(secondWord))*5;
+        if (textWidth(firstWord) < textWidth(secondWord)) {
+            speedFirst = (1.0 * width - textWidth(firstWord)) / (width - textWidth(secondWord)) * 5;
             speedSecond = -5;
         } else {
-            speedSecond = -(1.0*width-textWidth(secondWord))/(width-textWidth(firstWord))*5;
+            speedSecond = -(1.0 * width - textWidth(secondWord)) / (width - textWidth(firstWord)) * 5;
             speedFirst = 5;
         }
     }
@@ -34,20 +35,20 @@ public class Main extends PApplet {
         background(0);
 
         //first word
-        fill(255,0,0);
+        fill(255, 0, 0);
         text(firstWord, firstX, height / 4f);
 
         //second word
-        fill(0,255,0);
+        fill(0, 255, 0);
         text(secondWord, secondX, 3 * height / 4f);
 
         firstX += speedFirst;
         secondX += speedSecond;
 
-        if (firstX > width-textWidth(firstWord) || firstX < 0) {
+        if (firstX > width - textWidth(firstWord) || firstX < 0) {
             speedFirst *= (-1);
         }
-        if (secondX > width-textWidth(secondWord) || secondX < 0) {
+        if (secondX > width - textWidth(secondWord) || secondX < 0) {
             speedSecond *= (-1);
         }
     }
