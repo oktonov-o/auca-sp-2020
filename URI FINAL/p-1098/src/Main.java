c37070f37c027a5be0618a9067384a11589c6756
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-07 14:00:42 +0600

    URI p-1098 accepted

--- URI FINAL/p-1098/src/Main.java
+++ URI FINAL/p-1098/src/Main.java
@@ -2,11 +2,19 @@ public class Main {
     public static void main(String[] args) {
         for (int j = 0; j<=10; j++){
             float i = 1 + j/5f;
-            System.out.println("I="+j/5f+" J="+ i);
-            i = 2+ j/5f;
-            System.out.println("I="+j/5f+" J="+ i);
-            i = 3 + j/5f;
-            System.out.println("I="+j/5f+" J="+ i);
+            if ((int)i == i){
+                System.out.println("I="+j/5+" J="+ (int)i);
+                i = 2+ j/5f;
+                System.out.println("I="+j/5+" J="+ (int)i);
+                i = 3 + j/5f;
+                System.out.println("I="+j/5+" J="+ (int)i);
+            } else {
+                System.out.println("I="+j/5f+" J="+ i);
+                i = 2+ j/5f;
+                System.out.println("I="+j/5f+" J="+ i);
+                i = 3 + j/5f;
+                System.out.println("I="+j/5f+" J="+ i);
+            }
         }
     }
 }
