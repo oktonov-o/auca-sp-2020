7aae2d93a0828525ba14e282d6f7a73666649b0c
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-09 19:00:24 +0600

    lab06 Problem02 final

--- lab-06/src/Problem02.java
+++ lab-06/src/Problem02.java
@@ -10,6 +10,28 @@ public class Problem02 {
         System.out.print("Month: ");
         int month = inp.nextInt();
 
-        System.out.println();
+        System.out.println(getDaysOfMonth(year, month));
+    }
+
+    private static int getDaysOfMonth(int year, int month) {
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            return 31;
+        } else if (month == 2){
+            if (isLeapYear(year)){
+                return 29;
+            } else {
+                return 28;
+            }
+        } else {
+            return 30;
+        }
+    }
+
+    private static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
