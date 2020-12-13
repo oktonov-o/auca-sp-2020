a27741d1b67b1d14bd75ef80d45878e4cd1029fb
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-13 19:48:01 +0600

    lab07 Problem08 solved

---
+++ lab-07/src/Problem09.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Problem09 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        double[] myArr = new double[100];
+        int index = 0;
+
+        while (inp.hasNext()){
+            double x = inp.nextDouble();
+            myArr[index] = x;
+            index++;
+        }
+
+        double min = myArr[0];
+        int minIndex=-1;
+
+        for (int i = index-1; i>=0; i--){
+            if (min >= myArr[i]){
+                min = myArr[i];
+                minIndex = i;
+            }
+        }
+        System.out.println("Index of smallest number: "+minIndex);
+    }
+}
