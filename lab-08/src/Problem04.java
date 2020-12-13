090fe1fe5eafb25b4e61c60fe3f2195997ab4859
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-13 22:05:42 +0600

    lab08 Problem04 solved

--- lab-08/src/Problem04.java
+++ lab-08/src/Problem04.java
@@ -17,7 +17,7 @@ public class Problem04 {
         int[][] grid = new int[9][9];
         for (int i = 0; i < 9; i++)
             for (int j = 0; j < 9; j++)
-                grid[i][j] = input.nextInt();
+                grid[i][j] =input.nextInt();
         return grid;
     }
 
