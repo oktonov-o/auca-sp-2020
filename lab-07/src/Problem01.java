f92c679d6908245f4baed794bad665fa863cd2bb
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-13 17:30:43 +0600

    lab07 Problem01 solved

--- lab-07/src/Problem01.java
+++ lab-07/src/Problem01.java
@@ -10,12 +10,15 @@ public class Problem01 {
         Scanner inp = new Scanner(System.in);
         Random rnd = new Random();
 
+        System.out.print("N: ");
         int num = inp.nextInt();
 
         int[] myArray = new int[11];
 
         for (int i = 0; i<num; i++){
-            myArray[rnd.nextInt(11)]++;
+            int first = 1+ rnd.nextInt(6);
+            int second = 1+ rnd.nextInt(6);
+            myArray[first+second-2]++;
         }
         for (int i = 0; i<11; i++){
             System.out.println((i+2)+": "+myArray[i]);
