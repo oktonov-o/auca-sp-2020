3d7a0b2fd638160ca247e60538af4c80b278c405
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-19 15:34:43 +0600

    Project01 Problem08 solved

---
+++ Project01/Problem08/src/Main.java
@@ -0,0 +1,109 @@
+import processing.core.*;
+import javax.swing.JOptionPane;
+
+public class Main extends PApplet {
+
+    float r0;
+    float xo;
+    float yo;
+    float rs;
+    float xs;
+    float ys;
+
+    float angle;
+    float angle2;
+    float dAngle = 0.05f;
+    int numOfStars;
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+        String str = JOptionPane.showInputDialog("Number of stars: ");
+        numOfStars = Integer.parseInt(str);
+
+        r0 = min(width, height)/6f;
+        rs = r0/2f;
+    }
+
+    public void draw() {
+        background(0);
+
+        translate(width/2f, height/2f);
+        rotate(angle);
+        drawStar(r0, xo, yo, 255, 0, 0);
+
+        pushMatrix();
+        pushMatrix();
+        pushMatrix();
+
+        translate(0,2*r0);
+        rotate(angle2);
+        drawStar(rs, xs, ys, 0, 0, 255);
+
+        popMatrix();
+        translate(0,-2*r0);
+        rotate(angle2);
+        drawStar(rs, xs, ys, 0, 0, 255);
+
+        popMatrix();
+        translate(2*r0, 0);
+        rotate(angle2);
+        drawStar(rs, xs, ys, 0, 0, 255);
+
+        popMatrix();
+        translate(-2*r0, 0);
+        rotate(angle2);
+        drawStar(rs, xs, ys, 0, 0, 255);
+
+        angle += dAngle;
+        angle2 -= 2*dAngle;
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+    public void drawStar(float r, float x0, float y0, float redF, float greenF, float blueF) {
+        float xLeft = x0 - r;
+        float yLeft = y0;
+
+        float xRight = x0 + r;
+        float yRight = y0;
+
+        float xTop = x0;
+        float yTop = y0 - r;
+
+        float xBott = x0;
+        float yBott = y0 + r;
+
+        float xTL = x0 - r /4f;
+        float yTL = y0 - r /4f;
+
+        float xBL = x0 - r /4f;
+        float yBL = y0 + r /4f;
+
+        float xTR = x0 + r /4f;
+        float yTR = y0 - r /4f;
+
+        float xBR = x0 + r /4f;
+        float yBR = y0 + r /4f;
+
+        stroke(redF , greenF, blueF);
+        line(xLeft, yLeft, xRight, yRight);
+        line(xBott, yBott, xTop, yTop);
+
+        line(xTL, yTL, xBR, yBR);
+        line(xBL, yBL, xTR, yTR);
+
+        line(xLeft,yLeft,xTL,yTL);
+        line(xTop,yTop,xTL,yTL);
+        line(xLeft,yLeft,xBL,yBL);
+        line(xBott,yBott,xBL,yBL);
+        line(xBott,yBott,xBR,yBR);
+        line(xRight,yRight,xBR,yBR);
+        line(xTop,yTop,xTR,yTR);
+        line(xRight,yRight,xTR,yTR);
+    }
+}
