dfffbca04a25c81ecb8abfcaf0f972f01ef3bbe2
Orozkul Oktonov <oktonov_o@auca.kg>
2020-12-21 21:52:22 +0600

    Project02 Problem02 solved (different sizes, different speeds)

--- Project02/Problem02/src/Main.java
+++ Project02/Problem02/src/Main.java
@@ -1,4 +1,5 @@
 import processing.core.*;
+
 import javax.swing.JOptionPane;
 import java.util.Random;
 
@@ -31,13 +32,13 @@ public class Main extends PApplet {
 
         float x = 0.05f;
 
-        for (int i=0; i<numOfStars; i++) {
-            xOfStars[i] = rnd.nextInt(width-100)+10;
-            yOfStars[i] = rnd.nextInt(height-100)+10;
-            speedY[i] = rnd.nextInt(5)+1;
-            radiusOfStars[i] = rnd.nextInt(10)+10;
-            angles[i]=0;
-            dAngles[i]= x;
+        for (int i = 0; i < numOfStars; i++) {
+            xOfStars[i] = rnd.nextInt(width - 100) + 10;
+            yOfStars[i] = rnd.nextInt(height - 100) + 10;
+            speedY[i] = rnd.nextInt(5) + 1;
+            radiusOfStars[i] = rnd.nextInt(10) + 10;
+            angles[i] = 0;
+            dAngles[i] = x;
             x *= -1;
         }
 
@@ -45,7 +46,7 @@ public class Main extends PApplet {
 
     public void draw() {
         background(0);
-        for (int i=0; i<numOfStars; i++){
+        for (int i = 0; i < numOfStars; i++) {
             pushMatrix();
             translate(xOfStars[i], yOfStars[i]);
             rotate(angles[i]);
@@ -53,10 +54,10 @@ public class Main extends PApplet {
             popMatrix();
 
             yOfStars[i] += speedY[i];
-            angles[i]+= dAngles[i];
+            angles[i] += dAngles[i];
 
-            if (yOfStars[i]>height+2*radiusOfStars[i]){
-                yOfStars[i]= -2*radiusOfStars[i];
+            if (yOfStars[i] > height + 2 * radiusOfStars[i]) {
+                yOfStars[i] = -2 * radiusOfStars[i];
                 xOfStars[i] = rnd.nextInt(width);
             }
         }
@@ -69,20 +70,20 @@ public class Main extends PApplet {
     private void drawStar(float r) {
         stroke(255);
         strokeWeight(3);
-        line(0,-r, 0, r);
+        line(0, -r, 0, r);
 
         pushMatrix();
         pushMatrix();
 
-        rotate(3.14f/2);
-        line(0,-r, 0, r);
+        rotate(3.14f / 2);
+        line(0, -r, 0, r);
 
         popMatrix();
-        rotate(3.14f/4);
-        line(0,-sqrt(2*r*r), 0, sqrt(2*r*r));
+        rotate(3.14f / 4);
+        line(0, -sqrt(2 * r * r), 0, sqrt(2 * r * r));
 
         popMatrix();
-        rotate(3.14f/4*3);
-        line(0,-sqrt(2*r*r), 0, sqrt(2*r*r));
+        rotate(3.14f / 4 * 3);
+        line(0, -sqrt(2 * r * r), 0, sqrt(2 * r * r));
     }
 }
